2efe76ff17224a99073b93c0b4febe77ae32b827
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2023-04-06 12:33:19 -0400

    Updating welcome message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Welcome to Tanzu App Platform workshop at GP!";
+		return "Welcome to Tanzu App Platform!";
 	}
 
 }
