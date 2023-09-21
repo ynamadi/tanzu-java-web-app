3eb62db2adda3a548ed72b40578020514ebee0e8
Yunusa Namadi <98776848+ynamadi@users.noreply.github.com>
2023-09-21 11:37:07 -0400

    Test

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Welcome to Tanzu Build Service!";
+		return "Welcome to Tanzu Build Service Test!";
 	}
 
 }
